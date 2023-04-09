1577331c4dc71a7c733b0efc68978d7fec0c3730
sazzadi-r14 <sazzadi.cc9@gmail.com>
2023-04-08 17:43:55 -0700

    Initiation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Testing GIt update</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <title>HTML Elements Reference</title>
-      </header>
+    <div>
+        <h1>CS Bridge</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
